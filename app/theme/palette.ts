8c2fc437aa8c3ee6dfc959de28fbe93f46fa2adf
Jose <dreamzzediting@gmail.com>
2020-09-24 14:06:22 -0400

    Updated App palette

--- app/theme/palette.ts
+++ app/theme/palette.ts
@@ -2,9 +2,9 @@ export const palette = {
   black: "#1d1d1d",
   white: "#ffffff",
   offWhite: "#e6e6e6",
-  orange: "#FBA928",
-  orangeDarker: "#EB9918",
+  cyan: "#00AED4",
   lightGrey: "#939AA4",
   lighterGrey: "#CDD4DA",
-  angry: "#dd3333",
+  angry: "#E32828",
+  success: "#00CA14",
 }
